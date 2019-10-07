62683a8a288525c73a63bb9bbb642a2936b7a07c
EC2 Default User <ec2-user@ip-10-10-3-143.ap-northeast-1.compute.internal>
2019-10-07 09:08:55 +0000

    go pullreq

--- main.js
+++ main.js
@@ -20,13 +20,48 @@
 
 
 // ここでmyArray.forEachを実行して、console.logを使って各値とインデックスを出力する
+const myArray = {
+    array: [1, 2, 3],
+    forEach: function (callback) {
+        for (let i = 0; i < this.array.length; i++) {
+            callback(this.array[i], i);
+        }
+    },
+    map: function (callback) {
+        const mapArray = [];
+        this.forEach((value, index) => {
+            mapArray.push(callback(value, index));
+        });
+        return mapArray;
+    },
+    filter: function (callback) {
+        const filterArray = [];
+        this.forEach((value, index) => {
+            if (callback(value, index)) {
+                filterArray.push(value);
+            }
+        });
+        return filterArray;
+    }
+};
 
+console.log(myArray);
 
 // ここでmyArray.mapを実行したあと、mapの戻り値とmyArray.arrayをconsole.logで出力する
 // mapメソッドで取得する値は、myArray.arrayの各要素をindex倍した値とする。(value * index)
 // mapの戻り値は[0, 2, 6], myArray.arrayの値は[1, 2, 3]であるべき
-
+console.log(myArray.array);
+// myArray.forEach((value, index) => {console.log(value * index);});
+const mapArray = myArray.map((value, index) => {
+    return value * index;
+});
+console.log(mapArray);
 
 //ここでfilterを実行したあと、filterの戻り値とmyArray.arrayをconsole.logで出力する
 // filterメソッドで取得する値は、myArray.arrayの奇数だけとする
 // filterの戻り値は[1, 3], myArray.arrayの値は[1, 2, 3]であるべき
+console.log(myArray.array);
+const filterArray = myArray.filter((value, index) => {
+    return value % 2 === 1;
+});
+console.log(filterArray);
